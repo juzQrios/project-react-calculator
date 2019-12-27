fb836c3243df535b3e3c7ec03a0a48d890523c03
juzQrios <dj30c.1@gmail.com>
2019-12-27 23:12:45 +0530

    Refactor ButtonPanel render method

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,37 +3,28 @@ import Button from './Button';
 
 class ButtonPanel extends React.PureComponent {
   render() {
+    const buttonNames = {
+      'group-1': ['AC', '+/-', '%', '÷'],
+      'group-2': ['7', '8', '9', 'X'],
+      'group-3': ['4', '5', '6', '-'],
+      'group-4': ['1', '2', '3', '+'],
+      'group-5': ['0', '.', '+', ''],
+    };
+
     return (
+
       <div className="ButtonPanel">
-        <div id="group-1">
-          <Button name="AC" />
-          <Button name="+/-" />
-          <Button name="%" />
-          <Button name="÷" />
-        </div>
-        <div id="group-2">
-          <Button name="7" />
-          <Button name="8" />
-          <Button name="9" />
-          <Button name="X" />
-        </div>
-        <div id="group-3">
-          <Button name="4" />
-          <Button name="5" />
-          <Button name="6" />
-          <Button name="-" />
-        </div>
-        <div id="group-4">
-          <Button name="1" />
-          <Button name="2" />
-          <Button name="3" />
-          <Button name="+" />
-        </div>
-        <div id="group-5">
-          <Button name="0" />
-          <Button name="." />
-          <Button name="=" />
-        </div>
+        {
+          Object.keys(buttonNames).map((groupName) => (
+            <div id={groupName} key={groupName}>
+              {
+                buttonNames[groupName].map((buttonName) => (
+                  buttonName ? <Button name={buttonName} key={buttonName} /> : ''
+                ))
+              }
+            </div>
+          ))
+        }
       </div>
     );
   }
