7498f35e52d1b5ebab72ed1b0b38d3bd3545a8a1
juzQrios <dj30c.1@gmail.com>
2019-12-27 23:19:25 +0530

    Refactor Button as a function component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Button extends React.PureComponent {
-  render() {
-    const { name } = this.props;
-    return (
-      <button type="button">{ name }</button>
-    );
-  }
-}
+const Button = ({ name }) => (<button type="button">{ name }</button>);
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
