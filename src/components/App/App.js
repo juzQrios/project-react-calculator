33917388961996eb6c00c80ade7de97e473ec594
juzQrios <dj30c.1@gmail.com>
2019-12-30 21:33:40 +0530

    Add App component styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import './App.css';
 import Display from '../Display/Display';
 import ButtonPanel from '../ButtonPanel/ButtonPanel';
 
 class App extends React.Component {
   render() {
     return (
-      <div id="App" className="App">
+      <div className="App">
         <Display />
         <ButtonPanel />
       </div>
