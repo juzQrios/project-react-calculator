40526fb8196ce15450bd4465d5ae229e3f499552
juzQrios <dj30c.1@gmail.com>
2019-12-30 21:34:28 +0530

    Add Button component styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Button.css';
 
-const Button = ({ name }) => (<button type="button">{ name }</button>);
+const Button = ({ name, color, wide }) => (
+  <button
+    type="button"
+    className={`Button ${wide ? 'btn-wide' : 'btn-normal'} ${color}`}
+  >
+    { name }
+  </button>
+);
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: 'orange',
+  wide: false,
 };
 
 export default Button;
