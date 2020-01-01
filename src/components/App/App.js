38fc38767ca65e848f1b11ebf1e4c7d01e76c449
juzQrios <dj30c.1@gmail.com>
2020-01-01 11:16:26 +0530

    Add calculate.js to the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,17 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import calculate from '../../logic/calculate';
 import './App.css';
 import Display from '../Display/Display';
 import ButtonPanel from '../ButtonPanel/ButtonPanel';
 
 class App extends React.Component {
   render() {
+    const calculatorData = { total: null, next: null, operation: null };
+    const { total, next } = calculate(calculatorData, '0');
     return (
       <div className="App">
-        <Display />
+        <Display result={total || next} />
         <ButtonPanel />
       </div>
     );
